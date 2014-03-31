ab15a139610e65325e687fce82a3e9bde11916ce
Laban Eilers <laban@eilers.com>
2014-03-31 07:30:47 -0400

    Fixed bug in isHashedFile

--- test/hashpattern.unittests.js
+++ test/hashpattern.unittests.js
@@ -1,14 +1,26 @@
 "use strict";
 
-var assert = require("assert");
+var assert = require("chai").assert;
 var hashpattern = require("../lib/hashpattern");
 
 describe("hashpattern", function () {
 
     describe("#isHashedFile()", function () {
 
-        it("should match a full hashed path", function () {
-            assert.ok(hashpattern.isHashedFile("/foo/bar/baz/blah-hc12345.js"));
+        it("should return true for a full hashed path", function () {
+            assert.isTrue(hashpattern.isHashedFile("/foo/bar/baz/blah-hc12345.js"));
+        });
+
+        it("should return true for a hashed file name", function () {
+            assert.isTrue(hashpattern.isHashedFile("blah-hc12345.js"));
+        });
+
+        it("should return false for a full, non-hashed path", function () {
+            assert.isFalse(hashpattern.isHashedFile("/foo/bar/baz/blah.js"));
+        });
+
+        it("should return false for a non-hashed filename", function () {
+            assert.isFalse(hashpattern.isHashedFile("blah.js"));
         });
 
     });
