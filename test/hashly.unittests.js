e0fd24c16f9eb78028950c96cab34a6b5d7ddce1
Laban Eilers <laban@eilers.com>
2014-04-05 21:41:53 -0400

    Unit test for hashly.processDirectory()

--- test/hashly.unittests.js
+++ test/hashly.unittests.js
@@ -183,4 +183,68 @@ describe("hashly", function () {
         });
     });
 
+    describe("#processDirectory()", function () {
+
+        it("should call writeFileSync with manifest data", function () {
+            var hashly = rewire("../lib/hashly");
+
+            var options = {
+                manifestFormat: "json"
+            };
+
+            hashly.__set__("serializerFactory", {
+                getSerializer: function (manifestFormat) {
+                    assert.equal(manifestFormat, "json");
+                    return {
+                        serialize: function (trimmedManifest) {
+                            assert.isArray(trimmedManifest);
+                            assert.equal(trimmedManifest.length, 1);
+                            assert.equal(trimmedManifest[0].path, "/c/foo.png");
+                            return "abcdefg";
+                        }
+                    };
+                }
+            });
+
+            hashly.__set__("fsutil", {
+                existsSync: function (file) {
+                    assert.equal(file, "/a/b");
+                    return true;
+                },
+                deleteSync: function (file) {
+                    assert.equal(file, "/alt/b/manifest.json");
+                },
+                writeFileSync: function (file, data) {
+                    assert.equal(file, "/alt/b/manifest.json");
+                    assert.equal(data, "abcdefg");
+                }
+            });
+
+            hashly.__set__("getManifestPath", function (targetDir, serializer) {
+                assert.equal(targetDir, "/alt/b");
+                assert.property(serializer, "serialize");
+
+                return "/alt/b/manifest.json";
+            });
+
+            hashly.__set__("createManifestForDirectory", function (sourceDir, targetDir) {
+                assert.equal(sourceDir, "/a/b");
+                assert.equal(targetDir, "/alt/b");
+
+                return {
+                    manifest: [{
+                        path: "/c/foo.png",
+                        pathPhysical: "/a/b/c/foo.png",
+                        hashedPath: "/c/foo-hc-12345.png",
+                        hashedPathPhysical: "/a/b/c/foo-hc12345.png",
+                    }],
+                    errors: []
+                };
+            });
+
+            var exitCode = hashly.processDirectory("/a/b", "/alt/b", options);
+
+            assert.equal(exitCode, 0);
+        });
+    });
 });
