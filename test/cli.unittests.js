801ea189c644ae8222a66236abad00fea2a9f105
Laban Eilers <laban@eilers.com>
2014-05-27 19:57:13 -0400

    More unit tests for the cli library

--- test/cli.unittests.js
+++ test/cli.unittests.js
@@ -3,18 +3,51 @@
 var assert = require("chai").assert;
 var rewire = require("rewire");
 
-describe("hashly-cli", function () {
+describe("cli", function () {
 
     describe("#processArgs()", function () {
 
-        it("should return clean if --clean passed", function () {
+        var runProcessArgs = function (args, rootPath) {
             var cli = rewire("../lib/cli");
-            var argv = ["./", "./sourcedir", "--clean"];
+            var argv = ["/nodepath/node", "/hashlypath/hashly"].concat(args);
             var processArgs = cli.__get__("processArgs");
 
-            var result = processArgs(argv, "/a/b/c");
+            return processArgs(argv, rootPath || "/a/b/c");
+        };
+
+        it("should return clean args if --clean passed", function () {
+            var result = runProcessArgs(["./sourcedir", "--clean"]);
 
             assert.isTrue(result.clean);
+            assert.equal(result.sourceDir, "/a/b/c/sourcedir");
+            assert.equal(result.targetDir, "/a/b/c/sourcedir");
+        });
+
+        it("should return targetDir if passed 2 path args", function () {
+            var result = runProcessArgs(["./sourcedir", "./targetdir"]);
+
+            assert.isFalse(result.clean);
+            assert.equal(result.sourceDir, "/a/b/c/sourcedir");
+            assert.equal(result.targetDir, "/a/b/c/targetdir");
+        });
+
+        it("should return exclude/include args", function () {
+            var result = runProcessArgs(["./sourcedir", "--include=*.png,*.jpg", "--exclude=*.xml,*.config"]);
+
+            assert.isFalse(result.clean);
+            assert.equal(result.options.include, "*.png,*.jpg");
+            assert.equal(result.options.exclude, "*.xml,*.config");
+            assert.equal(result.sourceDir, "/a/b/c/sourcedir");
+            assert.equal(result.targetDir, "/a/b/c/sourcedir");
+        });
+
+        it("should return exclude/include args", function () {
+            var result = runProcessArgs(["./sourcedir", "--plugins=./pluginsdir"]);
+
+            assert.isFalse(result.clean);
+            assert.equal(result.plugins, "./pluginsdir");
+            assert.equal(result.sourceDir, "/a/b/c/sourcedir");
+            assert.equal(result.targetDir, "/a/b/c/sourcedir");
         });
     });
 });
